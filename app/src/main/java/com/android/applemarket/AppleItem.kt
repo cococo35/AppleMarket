c229ae3af3227bd9287f86f7e8888d1eb1c59bef
cococo35 <taejun0513@gmail.com>
2024-04-12 11:54:27 +0900

    add: appleitem.xml 추가

---
+++ app/src/main/java/com/android/applemarket/AppleItem.kt
@@ -0,0 +1,3 @@
+package com.android.applemarket
+
+data class AppleItem(val appleImg: Int, val appleName: String, val appleIntro: String, val appleSeller: String, val applePrice: String, val appleAddress: String, val appleHeart: Int, val appleChat: Int)
--- app/src/main/java/com/android/applemarket/MainActivity.kt
+++ app/src/main/java/com/android/applemarket/MainActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+        val appleDataList = mutableListOf<AppleItem>()
+        appleDataList.add(AppleItem(R.drawable.sample1, getString(R.string.sample1_name), getString(R.string.sample1_intro), getString(R.string.sample1_seller), getString(R.string.sample1_price), getString(R.string.sample1_address), getString(R.string.sample1_heart).toInt(), getString(R.string.sample1_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample2, getString(R.string.sample2_name), getString(R.string.sample2_intro), getString(R.string.sample2_seller), getString(R.string.sample2_price), getString(R.string.sample2_address), getString(R.string.sample2_heart).toInt(), getString(R.string.sample2_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample3, getString(R.string.sample3_name), getString(R.string.sample3_intro), getString(R.string.sample3_seller), getString(R.string.sample3_price), getString(R.string.sample3_address), getString(R.string.sample3_heart).toInt(), getString(R.string.sample3_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample4, getString(R.string.sample4_name), getString(R.string.sample4_intro), getString(R.string.sample4_seller), getString(R.string.sample4_price), getString(R.string.sample4_address), getString(R.string.sample4_heart).toInt(), getString(R.string.sample4_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample5, getString(R.string.sample5_name), getString(R.string.sample5_intro), getString(R.string.sample5_seller), getString(R.string.sample5_price), getString(R.string.sample5_address), getString(R.string.sample5_heart).toInt(), getString(R.string.sample5_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample6, getString(R.string.sample6_name), getString(R.string.sample6_intro), getString(R.string.sample6_seller), getString(R.string.sample6_price), getString(R.string.sample6_address), getString(R.string.sample6_heart).toInt(), getString(R.string.sample6_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample7, getString(R.string.sample7_name), getString(R.string.sample7_intro), getString(R.string.sample7_seller), getString(R.string.sample7_price), getString(R.string.sample7_address), getString(R.string.sample7_heart).toInt(), getString(R.string.sample7_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample8, getString(R.string.sample8_name), getString(R.string.sample8_intro), getString(R.string.sample8_seller), getString(R.string.sample8_price), getString(R.string.sample8_address), getString(R.string.sample8_heart).toInt(), getString(R.string.sample8_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample9, getString(R.string.sample9_name), getString(R.string.sample9_intro), getString(R.string.sample9_seller), getString(R.string.sample9_price), getString(R.string.sample9_address), getString(R.string.sample9_heart).toInt(), getString(R.string.sample9_chat).toInt()))
+        appleDataList.add(AppleItem(R.drawable.sample10, getString(R.string.sample10_name), getString(R.string.sample10_intro), getString(R.string.sample10_seller), getString(R.string.sample10_price), getString(R.string.sample10_address), getString(R.string.sample10_heart).toInt(), getString(R.string.sample10_chat).toInt()))
     }
 }
